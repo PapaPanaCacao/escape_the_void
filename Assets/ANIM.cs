9ddea68c3f723601f6cc787fc0f2eb1da83c9d2f
Papa Pana <panagiotis.tsiapkolis@etu.u-bordeaux.fr>
2016-11-23 14:07:51 -0800

    Hope this works

---
+++ Assets/ANIM.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ANIM : MonoBehaviour
+{
+	public Sprite[] frames;
+	public SpriteRenderer sr;
+
+	public int loopStart, loopEnd;
+	public float time;
+	private float frameTime = 0.0f;
+	private int curFrame = 0;
+
+	public Sprite GetSprite()
+	{
+		frameTime += Time.deltaTime;
+		while (frameTime > time) {
+			frameTime -= time;
+			NextFrame ();
+		}
+
+		return GetCurFrame ();
+	}
+
+	public Sprite GetCurFrame()
+	{
+		return frames [curFrame];
+	}
+
+	private void NextFrame()
+	{
+		if (curFrame == loopEnd)
+			curFrame = loopStart;
+		else
+			curFrame++;
+	}
+
+	public void Reset()
+	{
+		curFrame = 0;
+		frameTime = 0.0f;
+	}
+}
---
+++ Assets/PersoAnim.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersoAnim : MonoBehaviour
+{
+	public SpriteRenderer sr;
+
+	public ANIM stand, run, rise, fall, dash;
+
+	public ANIM currentAnim;
+
+	public void Update()
+	{
+		currentAnim.sr.sprite = currentAnim.GetSprite ();
+	}
+
+	public void ChangeAnim(ANIM a)
+	{
+		if (a == currentAnim)
+			return;
+
+		currentAnim.gameObject.SetActive (false);
+		currentAnim.Reset ();
+
+		currentAnim = a;
+		currentAnim.gameObject.SetActive (true);
+	}
+}
